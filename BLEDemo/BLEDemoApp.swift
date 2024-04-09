4b51c91b740a682276b7caa1ed2809da7e5e7ac7
Tsubata Seisyu <seisyuu43@gmail.com>
2024-04-09 16:55:59 +0900

    Initial Commit

---
+++ BLEDemo/BLEDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLEDemoApp.swift
+//  BLEDemo
+//
+//  Created by seisyu.tsubata on 2024/04/09.
+//
+
+import SwiftUI
+
+@main
+struct BLEDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
